7b2d04760a81dc9c551cfd5c59a2cff25c718045
Promise Nwafor <promisenwafor955@gmail.com>
2024-05-09 03:46:31 +0100

    update starter files

---
+++ actions/login.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+
+export const login = async ( values: z.infer<typeof LoginSchema>,) => {
+    const validatedFields = LoginSchema.safeParse(values);
+
+    if (!validatedFields.success) {
+      return { error: "Invalid fields!" };
+    }
+    
+    return { success: "Confirmation email sent!" };
+}
